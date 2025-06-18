29d020e9cd56e40989b24d54aa5f3b16ef71b1ec
tonny <tonyarrey50@gmail.com>
2025-06-18 15:53:51 +0100

    added the order and tracking page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import CheckoutPage from './pages/CheckoutPage'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
-import './App.css'
+import "./App.css";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route index element={<HomePage/>} />
-      <Route path='checkout' element={<CheckoutPage/>} />
-
-    </Routes>
-    
-
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
